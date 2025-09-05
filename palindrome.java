cfaa01d813e64912922c6b3d73aad121e4eb3e91
Navya Rastogi <rastoginavya2006@gmail.com>
2025-09-05 20:25:18 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter a string or number: ");
+        String input = sc.nextLine();
+
+     
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+       
+        if (input.equalsIgnoreCase(reversed)) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
