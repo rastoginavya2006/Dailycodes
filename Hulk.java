81a4dd347cc51ae8f982997a9ee1625706ac99bb
Navya Rastogi <rastoginavya2006@gmail.com>
2026-03-07 22:33:04 +0530

    Add files via upload

---
+++ Hulk.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Hulk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print("I hate ");
+            } else {
+                System.out.print("I love ");
+            }
+            
+            if (i == n) {
+                System.out.print("it");
+            } else {
+                System.out.print("that ");
+            }
+        }
+        
+        sc.close();
+    }
+}
