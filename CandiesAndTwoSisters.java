9684a7edab4ee83531ae3d095b2764d3fcb237dd
Navya Rastogi <rastoginavya2006@gmail.com>
2026-03-17 22:57:56 +0530

    Add files via upload

---
+++ CandiesAndTwoSisters.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CandiesAndTwoSisters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long n = sc.nextLong();
+
+            if (n <= 2) {
+                System.out.println(0);
+            } else {
+                System.out.println((n - 1) / 2);
+            }
+        }
+
+        sc.close();
+    }
+}
