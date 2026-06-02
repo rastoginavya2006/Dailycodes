e2ec1494dac33c525f142905816a10897d99f8ec
Navya Rastogi <rastoginavya2006@gmail.com>
2026-06-02 20:06:41 +0530

    Rename class from EasyProblem to EasyProblems

---
+++ EasyProblems.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EasyProblems {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt(); 
+            while (t-- > 0) {
+                int n = sc.nextInt();
+                
+                
+                System.out.println(n - 1);
+            }
+        }
+        
+        sc.close();
+    }
+}
