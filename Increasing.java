24754bed0f428a2e6bc9d7a5ef7fb1d2e9353b0c
Navya Rastogi <rastoginavya2006@gmail.com>
2026-06-03 22:21:55 +0530

    Implement Increasing sequence checker in Increasing.java

---
+++ Increasing.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Increasing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int n = sc.nextInt();
+                int[] a = new int[n];
+                
+                for (int i = 0; i < n; i++) {
+                    a[i] = sc.nextInt();
+                }
+                
+                Arrays.sort(a);
+                
+                boolean isStrictlyIncreasing = true;
+                
+                // Check for duplicates
+                for (int i = 1; i < n; i++) {
+                    if (a[i] == a[i - 1]) {
+                        isStrictlyIncreasing = false;
+                        break;
+                    }
+                }
+                
+                if (isStrictlyIncreasing) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+        
+        sc.close();
+    }
+}
