9a1afc27c726e2557a0af16ae82b4c3a013490df
Navya Rastogi <rastoginavya2006@gmail.com>
2026-06-16 22:45:01 +0530

    Add PlusOneOnTheSubset.java to calculate range

---
+++ PlusOneOnTheSubset.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class PlusOneOnTheSubset{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        while(t-- >0){
+            int n =sc.nextInt();
+            int[] arr = new int[n];
+            int min=Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+                if(arr[i]<min){
+                    min=arr[i];
+                }
+                if(arr[i]>max){
+                    max=arr[i];
+                }
+            }
+            System.out.println(max-min);
+            
+        }
+    }
+}
