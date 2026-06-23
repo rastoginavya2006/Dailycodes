3340fff1bed1eabfdb30468ad47111e94dac6e73
Navya Rastogi <rastoginavya2006@gmail.com>
2026-06-23 23:03:00 +0530

    Add MagicalSticks.java to calculate stick values

---
+++ MagicalSticks.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MagicalSticks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt(); 
+            while (t-- > 0) {
+                int n = sc.nextInt(); 
+                
+                System.out.println((n + 1) / 2);
+            }
+        }
+        
+        sc.close();
+    }
+}
