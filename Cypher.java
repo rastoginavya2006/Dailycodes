e5ca31833d0dab3b9c4b39b99a68c21253f03386
Navya Rastogi <rastoginavya2006@gmail.com>
2026-08-09 23:19:17 +0530

    Fix Scanner input source in Cypher.java

--- Cypher.java
+++ Cypher.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Cypher {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.util.System.in);
+        Scanner sc = new Scanner(System.in);
         
         
         if (!sc.hasNextInt()) return;
