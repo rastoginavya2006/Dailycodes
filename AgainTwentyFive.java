59862bd22f57eb72eb5dc6ab949e1a273a387f68
Navya Rastogi <rastoginavya2006@gmail.com>
2026-09-25 22:19:28 +0530

    Fix formatting and add newline at end of file

--- AgainTwentyFive.java
+++ AgainTwentyFive.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class AgainTwentyFive{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        long n=sc.nextLong();
+        long n=sc.nextLong(); 
         System.out.println(25);
     }
 }
